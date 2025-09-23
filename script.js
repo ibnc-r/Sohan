4504890bb2e486280199e9f16843d5ef7763384a
ibnc-r <premiumcard.4488@gmail.com>
2025-09-23 21:42:48 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function contactMe() {
             // Detect if the user is on an Android device
             if (/Android/.test(navigator.userAgent)) {
                 // Construct a mailto link with pre-filled details
-                window.location.href = "mailto:mdtorikulislam5748@gmail.com?subject=Contact%20Request&body=Hi%20Buddy!%20How%20are%20you...";
+                window.location.href = "mailto:mdsohanahmed2005@gmail.com?subject=Contact%20Request&body=Hi%20Buddy!%20How%20are%20you...";
             } else {
                 // Redirect to a specific URL (e.g., a contact form or email address)
                 window.location.href = "https://www.facebook.com/SohanAhmed.XXi";
